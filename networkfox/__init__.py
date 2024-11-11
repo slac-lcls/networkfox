12b414cb048917e3d8f71e87c37a49f5d77a7a6b
Seshu Yamajala <syamajala@gmail.com>
2024-11-11 10:41:42 -0500

    Bump version to 0.1

--- networkfox/__init__.py
+++ networkfox/__init__.py
@@ -3,7 +3,7 @@
 # Licensed under the terms of the Apache License, Version 2.0. See the LICENSE file associated with the project for terms.
 
 __author__ = 'hnguyen'
-__version__ = '0.0.9'
+__version__ = '0.1'
 
 from .functional import operation, compose, If, Else
 
